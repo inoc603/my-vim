eb3c9c77ec20c950bc5522e22f696deb49c012d8
inoc603 <inoc603@gmail.com>
2022-08-05 00:38:27 +0800

    refactor: use close-buffers

--- lua/init.lua
+++ lua/init.lua
@@ -1,3 +1,11 @@
+do
+    local close_buffers = require("close_buffers")
+    close_buffers.setup({})
+    vim.api.nvim_create_user_command('Bdi', function()
+        close_buffers.wipe({ type = 'hidden' })
+    end, { force = true })
+end
+
 require("config.lsp")
 require("config.lualine")
 require("config.coverage")
