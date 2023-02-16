61e3829531f2546513661b2254c95852eafe9235
inoc603 <inoc603@gmail.com>
2023-02-16 19:06:54 +0800

    feat: use prettier for js files

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -28,6 +28,7 @@ return {
             null_ls.setup({
                 sources = {
                     null_ls.builtins.formatting.black,
+                    null_ls.builtins.formatting.prettier,
                 },
             })
         end
